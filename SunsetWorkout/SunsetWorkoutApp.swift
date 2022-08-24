a430bf7ec297d83872bb8a5692d36ca3cfcb1b4d
OGGERO Paul <contact@poggero.com>
2022-08-24 11:29:36 +0200

    Initial Commit

---
+++ SunsetWorkout/SunsetWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunsetWorkoutApp.swift
+//  SunsetWorkout
+//
+//  Created by Paul Oggero on 24/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SunsetWorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
