7a0bf00bd3bf7528c042c635016c9b1e6310bb6e
OGGERO Paul <contact@poggero.com>
2022-08-25 12:26:25 +0200

    SwiftLint fixes

--- SunsetWorkoutUITests/SunsetWorkoutUITests.swift
+++ SunsetWorkoutUITests/SunsetWorkoutUITests.swift
@@ -18,7 +18,7 @@ class SunsetWorkoutUITests: XCTestCase {
         // In UI tests it’s important to set the initial state
         // - such as interface orientation - required for your tests before they run.
         // The setUp method is a good place to do this.
-        
+
         // UI tests must launch the application that they test.
         // Doing this in setup will make sure it happens for each test method.
         let app = XCUIApplication()
