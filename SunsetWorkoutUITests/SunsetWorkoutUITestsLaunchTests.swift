2d3cc51d97e92462e755db0b5ccefa4fe5f41961
OGGERO Paul <contact@poggero.com>
2022-08-25 12:13:14 +0200

    Added SnapshotHelper

--- SunsetWorkoutUITests/SunsetWorkoutUITestsLaunchTests.swift
+++ SunsetWorkoutUITests/SunsetWorkoutUITestsLaunchTests.swift
@@ -15,10 +15,17 @@ class SunsetWorkoutUITestsLaunchTests: XCTestCase {
 
     override func setUpWithError() throws {
         continueAfterFailure = false
+        // UI tests must launch the application that they test.
+        // Doing this in setup will make sure it happens for each test method.
+
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
     }
 
     func testLaunch() throws {
         let app = XCUIApplication()
+        snapshot("0_Launch")
         app.launch()
 
         // Insert steps here to perform after app launch but before taking a screenshot,
