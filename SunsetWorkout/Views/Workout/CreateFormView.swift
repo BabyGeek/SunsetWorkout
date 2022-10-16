3ae8bd08f4219825c8c9fbf54a1e3269ef316e9a
OGGERO Paul <contact@poggero.com>
2022-10-17 00:01:39 +0200

    feat(workout): add KeyboardReadable to create view and refactor

--- SunsetWorkout/Views/Workout/CreateFormView.swift
+++ SunsetWorkout/Views/Workout/CreateFormView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import HealthKit
 
-struct CreateFormView: View {
+struct CreateFormView: View, KeyboardReadable {
     @ObservedObject var workoutViewModel: WorkoutViewModel = WorkoutViewModel()
 
     @State private var type: SWWorkoutType = .highIntensityIntervalTraining
@@ -23,65 +23,75 @@ struct CreateFormView: View {
     @State var seriesNumber: String = ""
     @State var repetitionGoal: String = ""
 
+    @State var isKeyboardVisible: Bool = false
+
     var body: some View {
-        NavigationView {
-            VStack {
-                BaseWorkoutFormView(
-                    type: $type,
-                    name: $name,
-                    exerciseBreak: $exerciseBreak)
+        VStack {
+            BaseWorkoutFormView(
+                type: $type,
+                name: $name,
+                exerciseBreak: $exerciseBreak)
 
-                if type == .highIntensityIntervalTraining {
-                    HIITFormView(
-                        roundBreak: $roundBreak,
-                        roundNumber: $roundNumber)
-                }
-
-                if type == .traditionalStrengthTraining {
-                    TraditionalFormView(
-                        seriesBreak: $seriesBreak,
-                        seriesNumber: $seriesNumber,
-                        repetitionGoal: $repetitionGoal)
-                }
-
-                Spacer()
-
-                Button {
-                    workoutViewModel.workout = SWWorkout(name: name, type: type, metadata: [
-                        SWMetadata(type: .roundBreak, value: roundBreak),
-                        SWMetadata(type: .roundNumber, value: roundNumber),
-                        SWMetadata(type: .exerciseBreak, value: exerciseBreak),
-                        SWMetadata(type: .serieBreak, value: seriesBreak),
-                        SWMetadata(type: .serieNumber, value: seriesNumber),
-                        SWMetadata(type: .repetitionGoal, value: repetitionGoal)
-                    ])
-
-                    workoutViewModel.saveWorkout()
-
-                    if workoutViewModel.saved {
-                        name = ""
-                        roundBreak = ""
-                        roundNumber = ""
-                        exerciseBreak = ""
-                        seriesBreak = ""
-                        seriesNumber = ""
-                        repetitionGoal = ""
-
-                        workoutViewModel.saved = false
-                    }
-                } label: {
-                    RoundedRectangle(cornerRadius: 25)
-                        .frame(height: 50)
-                        .overlay(
-                            Text("Save")
-                                .foregroundColor(Color(.label))
-                        )
-                }
-
+            if type == .highIntensityIntervalTraining {
+                HIITFormView(
+                    roundBreak: $roundBreak,
+                    roundNumber: $roundNumber)
             }
-            .navigationTitle("New workout")
-            .padding()
+
+            if type == .traditionalStrengthTraining {
+                TraditionalFormView(
+                    seriesBreak: $seriesBreak,
+                    seriesNumber: $seriesNumber,
+                    repetitionGoal: $repetitionGoal)
+            }
+
+            Spacer()
+
+            if !isKeyboardVisible {
+                saveButton
+            }
         }
+        .padding()
+        .onReceive(keyboardPublisher) { newIsKeyboardVisible in
+            isKeyboardVisible = newIsKeyboardVisible
+        }
+    }
+}
+
+extension CreateFormView {
+    var saveButton: some View {
+        Button {
+            workoutViewModel.workout = SWWorkout(name: name, type: type, metadata: [
+                SWMetadata(type: .roundBreak, value: roundBreak),
+                SWMetadata(type: .roundNumber, value: roundNumber),
+                SWMetadata(type: .exerciseBreak, value: exerciseBreak),
+                SWMetadata(type: .serieBreak, value: seriesBreak),
+                SWMetadata(type: .serieNumber, value: seriesNumber),
+                SWMetadata(type: .repetitionGoal, value: repetitionGoal)
+            ])
+
+            workoutViewModel.saveWorkout()
+
+            if workoutViewModel.saved {
+                name = ""
+                roundBreak = ""
+                roundNumber = ""
+                exerciseBreak = ""
+                seriesBreak = ""
+                seriesNumber = ""
+                repetitionGoal = ""
+
+                workoutViewModel.saved = false
+            }
+        } label: {
+            RoundedRectangle(cornerRadius: 25)
+                .frame(height: 50)
+                .overlay(
+                    Text("Save")
+                        .foregroundColor(Color(.label))
+                )
+        }
+
     }
 }
 
