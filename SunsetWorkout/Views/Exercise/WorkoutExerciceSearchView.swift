b06043a3678f1139d7d1b6516c604c26390e62ad
OGGERO Paul <contact@poggero.com>
2022-10-17 23:02:06 +0200

    feat(workouts): WIP adding exercice search

---
+++ SunsetWorkout/Views/Exercise/WorkoutExerciceSearchView.swift
@@ -0,0 +1,22 @@
+//
+//  WorkoutExerciceSearchView.swift
+//  SunsetWorkout
+//
+//  Created by Paul Oggero on 17/10/2022.
+//
+
+import SwiftUI
+
+struct WorkoutExerciceSearchView: View {
+    var body: some View {
+        VStack {
+            Text("Searching...")
+        }
+    }
+}
+
+struct WorkoutExerciceSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        WorkoutExerciceSearchView()
+    }
+}
