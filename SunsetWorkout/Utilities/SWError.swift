138a9db67abce8e5b3e95a94f17fa47cc2cf61ff
OGGERO Paul <contact@poggero.com>
2022-10-20 23:09:28 +0200

    feat(errors): updated SWError to always have string description and failureReason

--- SunsetWorkout/Utilities/SWError.swift
+++ SunsetWorkout/Utilities/SWError.swift
@@ -10,4 +10,24 @@ import Foundation
 struct SWError: Identifiable {
     let id: UUID = UUID()
     let error: Error
+
+    var description: String {
+        if let error = error as? LocalizedError {
+            if let description = error.errorDescription {
+                return description
+            }
+        }
+
+        return GlobalError.unknown.errorDescription!
+    }
+
+    var failureReason: String {
+        if let error = error as? LocalizedError {
+            if let failureReason = error.failureReason {
+                return failureReason
+            }
+        }
+
+        return GlobalError.unknown.failureReason!
+    }
 }
